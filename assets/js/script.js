d4c3b5d3504797d375bbb54183302b7fb08f58ed
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-08-16 15:29:32 +0000

    tidy up for user, enter key active for submit and curser resets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Wait for the DOM to finish loading before running the game
-// Get the button elements and add even listeners to them
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 
 })
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
